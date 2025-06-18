7af212fe50784c9df9ffa550c23485c37b4bc0ee
Srikakulapu <2414342@cognizant.com>
2025-06-18 17:01:44 +0530

    ADDED NOTIFICATION MODULE FEATURES

---
+++ src/main/java/com/cts/library/controller/NotificationController.java
@@ -0,0 +1,40 @@
+package com.cts.library.controller;
+
+import com.cts.library.model.Notification;
+import com.cts.library.service.NotificationService;
+import com.cts.library.service.NotificationServiceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/notifications")
+public class NotificationController {
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @PostMapping
+    public Notification createNotification(@RequestBody Notification notification) {
+        return notificationService.createNotification(notification);
+    }
+
+    @GetMapping
+    public List<Notification> getAllNotifications() {
+        return notificationService.getAllNotifications();
+    }
+
+    @GetMapping("/{id}")
+    public Notification getNotification(@PathVariable Long id) {
+        return notificationService.getNotificationById(id);
+    }
+    
+    @GetMapping("/trigger")
+    public String triggerNotificationsManually() {
+        ((NotificationServiceImpl) notificationService).generateDueAndOverdueNotifications();
+        return "Notifications triggered.";
+    }
+
+}
